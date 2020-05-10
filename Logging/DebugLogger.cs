15fe2df014974f0d4d69268f71ef193a34577756
george-stroescu <geo_stroescu@yahoo.com>
2020-05-10 19:14:24 +0300

    Logger done

---
+++ Logging/DebugLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Logging
+{
+    class DebugLogger : Logger
+    {
+        public override void WriteLog(LogLevel logLevel, string errorMessage, string additionalInfo = null)
+        {
+            Debug.WriteLine($"{logLevel}: {errorMessage}");
+            if (!String.IsNullOrWhiteSpace(additionalInfo))
+            {
+                Debug.WriteLine($"{additionalInfo}");
+            }
+        }
+    }
+}
---
+++ Logging/Logger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logging
+{
+    public abstract class Logger
+    {
+        public abstract void WriteLog(LogLevel logLevel, string errorMessage, string additionalInfo = null);
+    }
+}
